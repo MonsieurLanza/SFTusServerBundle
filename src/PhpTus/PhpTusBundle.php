ce2a3016775686ce04bc3a31da1f2126cc9c07f1
Lanza <dev@lanza.fr>
2017-01-04 22:09:45 +0100

    PhpTusBundle creation & import

---
+++ src/PhpTus/PhpTusBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lanza
+ * Date: 04/01/17
+ * Time: 22:06
+ */
+
+namespace PhpTus;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class PhpTusBundle extends Bundle
+{
+}
